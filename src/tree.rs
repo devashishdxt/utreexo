9924ca39f5b0ce2e7ef571cc5b8674d4c34e3b58
Devashish Dixit <devashish@crypto.com>
2019-08-28 13:58:39 +0800

    Add tests for checking leaf hashes and paths for a tree

--- src/tree.rs
+++ src/tree.rs
@@ -1,9 +1,27 @@
-use alloc::{vec, vec::Vec};
+use alloc::vec::Vec;
 use core::convert::TryInto;
 
-use crate::{hash_intermediate, hash_many_leaves, Hash, Path};
+use crate::{hash_intermediate, Hash, Path};
 
 /// An owned `Vec` representing a merkle tree
+//
+// # Tree representation: Numbers are index/position of nodes in vector containing the full tree
+//
+//              14 <- Root hash
+//              / \
+//             /   \
+//            /     \
+//           /       \
+//          /         \
+//         /           \
+//        /             \
+//       6              13
+//      / \             / \
+//     /   \           /   \
+//    /     \         /     \
+//   2       5       9      12
+//  / \     / \     / \     / \
+// 0   1   3   4   7   8  10   11 <- Leaves
 #[derive(Debug)]
 #[repr(transparent)]
 pub struct Tree(pub(crate) Vec<Hash>);
@@ -107,7 +125,7 @@ impl<'a> TreeRef<'a> {
     /// Returns number of nodes in tree
     #[inline]
     pub fn nodes(&self) -> usize {
-        debug_assert!(self.nodes() % 2 == 1);
+        debug_assert!(self.0.len() % 2 == 1);
         self.0.len()
     }
 
@@ -170,5 +188,105 @@ impl<'a> TreeRef<'a> {
     #[inline]
     pub fn leaf_paths(&self) -> Vec<Path> {
         Path::for_height(self.height())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::{hash_leaf, Direction};
+
+    #[test]
+    fn check_leaves_and_height() {
+        let nodes = (0..15)
+            .map(|value: usize| hash_leaf(&value.to_be_bytes()))
+            .collect::<Vec<Hash>>();
+        let tree = Tree(nodes);
+
+        assert_eq!(15, tree.nodes());
+        assert_eq!(8, tree.leaves());
+        assert_eq!(3, tree.height());
+    }
+
+    #[test]
+    fn check_leaf_hashes() {
+        let nodes = (0..15)
+            .map(|value: usize| hash_leaf(&value.to_be_bytes()))
+            .collect::<Vec<Hash>>();
+        let manual_leaf_hashes = vec![
+            nodes[0], nodes[1], nodes[3], nodes[4], nodes[7], nodes[8], nodes[10], nodes[11],
+        ];
+
+        let tree = Tree(nodes);
+        let leaf_hashes = tree.leaf_hashes();
+
+        assert_eq!(leaf_hashes, manual_leaf_hashes);
+    }
+
+    #[test]
+    fn check_leaf_paths() {
+        let nodes = (0..15)
+            .map(|value: usize| hash_leaf(&value.to_be_bytes()))
+            .collect::<Vec<Hash>>();
+
+        let tree = Tree(nodes);
+        let leaf_paths = tree.leaf_paths();
+
+        let mut first_leaf_directions = leaf_paths[0].directions();
+
+        assert_eq!(Direction::Left, first_leaf_directions.next().unwrap());
+        assert_eq!(Direction::Left, first_leaf_directions.next().unwrap());
+        assert_eq!(Direction::Left, first_leaf_directions.next().unwrap());
+        assert!(first_leaf_directions.next().is_none());
+
+        let mut second_leaf_directions = leaf_paths[1].directions();
+
+        assert_eq!(Direction::Left, second_leaf_directions.next().unwrap());
+        assert_eq!(Direction::Left, second_leaf_directions.next().unwrap());
+        assert_eq!(Direction::Right, second_leaf_directions.next().unwrap());
+        assert!(second_leaf_directions.next().is_none());
+
+        let mut third_leaf_directions = leaf_paths[2].directions();
+
+        assert_eq!(Direction::Left, third_leaf_directions.next().unwrap());
+        assert_eq!(Direction::Right, third_leaf_directions.next().unwrap());
+        assert_eq!(Direction::Left, third_leaf_directions.next().unwrap());
+        assert!(third_leaf_directions.next().is_none());
+
+        let mut fourth_leaf_directions = leaf_paths[3].directions();
+
+        assert_eq!(Direction::Left, fourth_leaf_directions.next().unwrap());
+        assert_eq!(Direction::Right, fourth_leaf_directions.next().unwrap());
+        assert_eq!(Direction::Right, fourth_leaf_directions.next().unwrap());
+        assert!(fourth_leaf_directions.next().is_none());
+
+        let mut fifth_leaf_directions = leaf_paths[4].directions();
+
+        assert_eq!(Direction::Right, fifth_leaf_directions.next().unwrap());
+        assert_eq!(Direction::Left, fifth_leaf_directions.next().unwrap());
+        assert_eq!(Direction::Left, fifth_leaf_directions.next().unwrap());
+        assert!(fifth_leaf_directions.next().is_none());
+
+        let mut sixth_leaf_directions = leaf_paths[5].directions();
+
+        assert_eq!(Direction::Right, sixth_leaf_directions.next().unwrap());
+        assert_eq!(Direction::Left, sixth_leaf_directions.next().unwrap());
+        assert_eq!(Direction::Right, sixth_leaf_directions.next().unwrap());
+        assert!(sixth_leaf_directions.next().is_none());
+
+        let mut seventh_leaf_directions = leaf_paths[6].directions();
+
+        assert_eq!(Direction::Right, seventh_leaf_directions.next().unwrap());
+        assert_eq!(Direction::Right, seventh_leaf_directions.next().unwrap());
+        assert_eq!(Direction::Left, seventh_leaf_directions.next().unwrap());
+        assert!(seventh_leaf_directions.next().is_none());
+
+        let mut eighth_leaf_directions = leaf_paths[7].directions();
+
+        assert_eq!(Direction::Right, eighth_leaf_directions.next().unwrap());
+        assert_eq!(Direction::Right, eighth_leaf_directions.next().unwrap());
+        assert_eq!(Direction::Right, eighth_leaf_directions.next().unwrap());
+        assert!(eighth_leaf_directions.next().is_none());
     }
 }
